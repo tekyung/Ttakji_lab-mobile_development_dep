c322b22d0fb24eba5a31300f58c2ccff9e72661d
tekyung <103092384+tekyung@users.noreply.github.com>
2026-03-28 14:08:22 +0900

    gamerule 업데이트

--- Assets/TCG_Project/Scripts/Systems/GameRules.cs
+++ Assets/TCG_Project/Scripts/Systems/GameRules.cs
@@ -58,6 +58,8 @@ namespace TCG_Project.Scripts.Systems
         public static int LifeTokens => GetValue("life_tokens", 5);
         public static int ResourceDeckCount => GetValue("resource_deck_count", 15);
         public static float BotDelayTime => GetValue("Bot_delay_time", 500) / 1000f;
+        public static int BotSingleGame => GetValue("Bot_single_game", 1);
+        public static int WaitTime => GetValue("choose_wait_time", 10000);
     }
     
     // JSON 구조에 맞춘 래퍼 클래스
